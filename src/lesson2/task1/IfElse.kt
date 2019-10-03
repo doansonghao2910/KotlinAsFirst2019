076e65f5e240e01d55cb140406c539d80811c87d
doansonghao2910 <doansonghao2910@gmail.com>
2019-10-04 00:55:10 +0300

    lessons 1,2,3

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -122,7 +122,7 @@ fun rookOrBishopThreatens(
     rookX: Int, rookY: Int,
     bishopX: Int, bishopY: Int
 ): Int = when {
-    kingX == rookX || kingY == rookY && (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 3
+    (kingX == rookX || kingY == rookY) && (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 3
     kingX == rookX || kingY == rookY -> 1
     (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 2
     else -> 0
