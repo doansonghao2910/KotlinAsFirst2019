6971f8bd9344c117b920950f5aef0b4a58935468
doansonghao2910 <doansonghao2910@gmail.com>
2019-12-15 15:03:49 +0300

    lessons 1,2,3,4,5,6,7

--- src/lesson3/task1/SSS.kt
+++ src/lesson3/task1/SSS.kt
@@ -1,9 +1,11 @@
 package lesson3.task1
 
 fun main() {
-    val x = "Он пошёл в в школу"
-    var list = x.toLowerCase().split(" ")
-    println(list)
+    val x = "Хлеб 39.9; Молоко 62.5; Курица 184.0; Конфеты 89.9"
+    var list = x.split("; ", " ")
+    var b = mutableListOf<String>()
+    var a = mutableListOf<Double>()
+
 }
 
 
