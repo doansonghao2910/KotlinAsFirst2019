35de0a48067dedd90f40140064a3c535c77c2b53
doansonghao2910 <doansonghao2910@gmail.com>
2019-12-18 19:37:13 +0300

    1,2,3,4,5,6,7

--- src/lesson3/task1/SSS.kt
+++ src/lesson3/task1/SSS.kt
@@ -3,10 +3,9 @@ package lesson3.task1
 import lesson8.task1.Point
 
 fun main() {
-    var x = Point(1.0, 2.0)
-    var y = Point(3.0, 5.0)
-    println(x.distance(y))
-
+    println(
+        Regex("""[^\d\+\-\s\n\t]""").containsMatchIn("\n")
+    )
 
 }
 
